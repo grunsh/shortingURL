03a197f5dbc07bc497bdd8f8b4777f363be1c5ad
Alexander Grunin <avg44@mail.ru>
2023-10-09 00:03:33 +0300

    Адаптация в связи с изменениями во втором инкременте

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,3 +1,11 @@
+/*
+Дяденька, вот ты, который взялся смотреть мой код и ревью писать. Обращаюсь к тебе с просьбой, не будь сильно строг,
+ПОЖАЛУЙСТА!  Я не кодил почти 20 лет (было php/Perl во времена сисадминства, интернет тогда был dial-up ещё).
+Я попросил на работе купить мне курс Go. И как-то так получилось, что был куплен продвинутый курс, а не для тех, кто
+учится ходить. Для меня любой мало-мальски работающий код - победа, как для годовалого ребёнка первый десяток шагов
+держась за палец. Я не сдаюсь. Стараюсь. Я даже с гитом до курса не работал и никогда не писал юнит тестов. А тут вон чо.
+Спасибо тебе дяденька за понимание, заранее. Не заваливай пожалуйста. Я почти не сплю, но тяну лямку и грызу гранит.
+*/
 package main
 
 import (
@@ -12,15 +20,14 @@ const shortURLDomain string = "http://localhost:8080/"
 
 var urlStorage [][]string //слайс для хранения URL и их хешей, первый индекс - запись, второй: 0 - URL, 1 - хеш
 
-// Генератор сокращённого URL. Использует константу shortUrlDomain как настройку.
+// Генератор сокращённого URL. Использует константу shortURLDomain как настройку.
 func addURL(url []byte) []byte {
 	hashStr := getHash() // Сохраним хэш в переменную. Понадобится для сохранения в массиве и для формирования короткого URL
 	urlVar := make([]string, 0)
 	// Сформируем слайс-строку в слайс урлов. Колонка 0 - сокращённый URL, 1 - хеш
 	urlVar = append(urlVar, string(url))
 	urlVar = append(urlVar, hashStr)
-	// ...и сохраним её в слайс строк
-	urlStorage = append(urlStorage, urlVar)
+	urlStorage = append(urlStorage, urlVar) // ...и сохраним её в слайс строк
 	return []byte(shortURLDomain + hashStr)
 }
 
@@ -41,25 +48,30 @@ func shortingRequest(res http.ResponseWriter, req *http.Request) {
 		for i := len(urlStorage) - 1; i >= 0; i-- { //По слайсу идём с конца, ищем самый свежий редирект
 			if urlStorage[i][1] == id {
 				res.Header().Set("Location", urlStorage[i][0]) // Укажем куда редирект
+				res.Header().Set("Content-Type", "text/plain") // Установим тип ответа text/plain
 				res.WriteHeader(http.StatusTemporaryRedirect)  // Передаём 307
 				return                                         // Нашли нужный хеш и выдали редирект. Завершаем работу хендлера.
 			}
-		} //for...
-		res.WriteHeader(http.StatusBadRequest) // Прошли весь массив, но хеша нет. Ошибка 400
-		return                                 // Выход по 400
+		} //for... поиск хеша в памяти
+		res.Header().Set("Content-Type", "text/plain") // Установим тип ответа text/plain
+		res.WriteHeader(http.StatusBadRequest)         // Прошли весь массив, но хеша нет.
+		return                                         // Выход по 400
 	} else if req.Method == http.MethodPost {
 		data, err := io.ReadAll(req.Body)
+		req.Body.Close()
 		if err != nil {
+			res.Header().Set("Content-Type", "text/plain") // Установим тип ответа text/plain
 			res.WriteHeader(http.StatusBadRequest)
+			return // Выход по 400
 		}
 		shrtURL := addURL(data)
-		res.WriteHeader(http.StatusCreated)
 		res.Header().Set("Content-Type", "text/plain") // Установим тип ответа text/plain
 		res.Header().Set("Content-Length", strconv.Itoa(len(shrtURL)))
+		res.WriteHeader(http.StatusCreated)
 		res.Write(shrtURL)
-
 	} else {
-		res.WriteHeader(http.StatusBadRequest) // Ошибка запроса (не пост, и не гет)
+		res.Header().Set("Content-Type", "text/plain") // Установим тип ответа text/plain
+		res.WriteHeader(http.StatusBadRequest)         // Ошибка запроса (не пост, и не гет)
 	}
 
 }
