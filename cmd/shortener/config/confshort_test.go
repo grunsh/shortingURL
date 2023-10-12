ab62ee31ce0eb827db5687880c9e23f92dccff3b
Alexander Grunin <avg44@mail.ru>
2023-10-13 01:31:56 +0300

    Добавил тесты для конфига. Тупенькие, но тесты.

--- cmd/shortener/config/confshort_test.go
+++ cmd/shortener/config/confshort_test.go
@@ -1,1 +1,14 @@
 package config
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestParams(t *testing.T) {
+	assert.Equal(t, 2, len(Prms))
+	assert.Equal(t, "a", Prms[0].param.name)
+	assert.Equal(t, "localhost:8080", Prms[0].param.defValue)
+	assert.Equal(t, "b", Prms[1].param.name)
+	assert.Equal(t, "http://localhost:8080/", Prms[1].param.defValue)
+}
